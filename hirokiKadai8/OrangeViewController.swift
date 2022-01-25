dca20f3f49b736510a5f6fcf1acbc2fd660a83af
hecuhecu <hecunyuji5@gmail.com>
2022-01-25 11:24:27 +0900

    first commit

---
+++ hirokiKadai8/OrangeViewController.swift
@@ -0,0 +1,28 @@
+//
+//  OrangeViewController.swift
+//  hirokiKadai8
+//
+//  Created by 河村宇記 on 2022/01/24.
+//
+
+import UIKit
+
+class OrangeViewController: UIViewController {
+
+    @IBOutlet private weak var sliderValue: UISlider!
+    @IBOutlet private weak var sliderLabel: UILabel!
+    private let delegate = UIApplication.shared.delegate as! AppDelegate
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.sliderValue.value = delegate.sliderValue
+        self.sliderLabel.text = "\(delegate.sliderValue)"
+    }
+    
+    @IBAction private func changeSlider(_ sender: Any) {
+        delegate.sliderValue = self.sliderValue.value
+        self.sliderLabel.text = "\(self.sliderValue.value)"
+    }
+    
+}
