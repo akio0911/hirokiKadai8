32ee0488d13faf72a6b419beb860c967bc9d37a4
akio0911 <akio0911@gmail.com>
2022-01-26 17:28:08 +0900

    feedback

--- hirokiKadai8/GreenViewController.swift
+++ hirokiKadai8/GreenViewController.swift
@@ -9,20 +9,20 @@ import UIKit
 
 class GreenViewController: UIViewController {
 
-    @IBOutlet private weak var sliderValue: UISlider!
-    @IBOutlet private weak var sliderLabel: UILabel!
+    @IBOutlet private weak var valueSlider: UISlider!
+    @IBOutlet private weak var valueLabel: UILabel!
     private let delegate = UIApplication.shared.delegate as! AppDelegate
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        self.sliderValue.value = delegate.sliderValue
-        self.sliderLabel.text = "\(delegate.sliderValue)"
+        valueSlider.value = delegate.sliderValue
+        valueLabel.text = "\(delegate.sliderValue)"
     }
     
     @IBAction private func changeSlider(_ sender: Any) {
-        delegate.sliderValue = self.sliderValue.value
-        self.sliderLabel.text = "\(self.sliderValue.value)"
+        delegate.sliderValue = valueSlider.value
+        valueLabel.text = "\(valueSlider.value)"
     }
     
 }
